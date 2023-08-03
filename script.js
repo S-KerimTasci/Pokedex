1a34f7015e2cb8126b62ff491237edca0b317f46
Kerim <kerim.tasci@yahoo.de>
2023-08-03 10:46:52 +0200

    render

---
+++ script.js
@@ -0,0 +1,49 @@
+const first151PokemonNames = ["bulbasaur","ivysaur","venusaur","charmander","charmeleon","charizard","squirtle","wartortle","blastoise","caterpie","metapod","butterfree","weedle","kakuna","beedrill","pidgey","pidgeotto","pidgeot","rattata","raticate","spearow","fearow","ekans","arbok","pikachu","raichu","sandshrew","sandslash","nidoran-f","nidorina","nidoqueen","nidoran-m","nidorino","nidoking","clefairy","clefable","vulpix","ninetales","jigglypuff","wigglytuff","zubat","golbat","oddish","gloom","vileplume","paras","parasect","venonat","venomoth","diglett","dugtrio","meowth","persian","psyduck","golduck","mankey","primeape","growlithe","arcanine","poliwag","poliwhirl","poliwrath","abra","kadabra","alakazam","machop","machoke","machamp","bellsprout","weepinbell","victreebel","tentacool","tentacruel","geodude","graveler","golem","ponyta","rapidash","slowpoke","slowbro","magnemite","magneton","farfetchd","doduo","dodrio","seel","dewgong","grimer","muk","shellder","cloyster","gastly","haunter","gengar","onix","drowzee","hypno","krabby","kingler","voltorb","electrode","exeggcute","exeggutor","cubone","marowak","hitmonlee","hitmonchan","lickitung","koffing","weezing","rhyhorn","rhydon","chansey","tangela","kangaskhan","horsea","seadra","goldeen","seaking","staryu","starmie","mr-mime","scyther","jynx","electabuzz","magmar","pinsir","tauros","magikarp","gyarados","lapras","ditto","eevee","vaporeon","jolteon","flareon","porygon","omanyte","omastar","kabuto","kabutops","aerodactyl","snorlax","articuno","zapdos","moltres","dratini","dragonair","dragonite","mewtwo","mew"];
+let currentPokemon;
+
+
+async function loadPokemon(){
+    for (let i = 0; i < first151PokemonNames.length; i++) {
+        const element = first151PokemonNames[i];
+
+        let url = `https://pokeapi.co/api/v2/pokemon/${element}`;
+        let response = await fetch(url);
+        currentPokemon = await response.json();
+
+        if (currentPokemon) {
+            renderPokedex(i)
+        }else{
+            console.log(currentPokemon)
+        }
+    }
+
+    /*let url = 'https://pokeapi.co/api/v2/pokemon/charmander';
+    let response = await fetch(url);
+    currentPokemon = await response.json();
+
+    console.log(currentPokemon)
+    renderPokedex()*/
+}
+
+async function renderPokedex(i){
+
+    document.getElementById('pokedexContainer').innerHTML += /*html*/`    
+    <div class="pokedex">
+    <h1 id="name${i}">${currentPokemon['name']}</h1>
+    <img class="picture" src="${currentPokemon['sprites']['other']['home']['front_default']}" id="picture${i}" alt="">
+    <span id="type1.${i}">${currentPokemon['types']['0']['type']['name']}</span>
+    <span id="type2.${i}"></span>
+    </div>
+    `
+        
+    if (currentPokemon['types']['1']['type']['name']) {
+        document.getElementById(`type2.${i}`).innerHTML =  await currentPokemon['types']['1']['type']['name']
+    }
+
+
+
+
+
+ //   document.getElementById('name').innerHTML = currentPokemon['name'];
+   // document.getElementById('picture').src = currentPokemon['sprites']['other']['home']['front_default']
+}
